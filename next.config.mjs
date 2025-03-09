6825d7f1faf3e971d2f634e4a3eb82fe5d49f4da
Sai Rohith <54322833+rohith1125@users.noreply.github.com>
2025-03-09 18:41:51 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', 
+  basePath: '/code-diff-checker', 
+  images: {
+    unoptimized: true, 
+  },
+  assetPrefix: '/code-diff-checker/', 
 };
 
 export default nextConfig;
-
